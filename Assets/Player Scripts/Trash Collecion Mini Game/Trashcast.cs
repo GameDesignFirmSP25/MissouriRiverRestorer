f5dfc713c7b7e42458716778760a0c3f89e50810
AJS758 <146855739+AJS758@users.noreply.github.com>
2025-04-02 13:05:34 -0500

    Began Work on trash collection mini game
    Started work on trash collection mini game added folder system scripts with subscripts for audio

---
+++ Assets/Player Scripts/Trash Collecion Mini Game/Trashcast.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trashcast : MonoBehaviour
+{
+    public Camera _mainCamera;
+
+    private Ray _ray;
+    private RaycastHit _hit;
+
+    public bool TrashClicked = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    public void RiverClicked() // Raycast to detect when the mouse clicks the River
+    {
+        // Detect mouse click.
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hit))
+            {
+                Debug.Log("Trash Collected");
+                TrashClicked = true; // Set bool riverClicked to true
+
+            }
+        }
+    }
+}
