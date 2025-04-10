691f5184e8475fc29f68275fb1ecfe3c086a2d07
smrjewell <smrjewell@gmail.com>
2025-04-10 16:11:13 -0500

    Water Testing MiniGame  functionality
    fixed issue of multiple panels being able to be up at the same time
    
    fixed issue of prefabs activate panels more than once
    
    fixed instructions panel functionality

--- Assets/Scripts/Mini Game Scripts/Water Testing Mini Game/Raycast.cs
+++ Assets/Scripts/Mini Game Scripts/Water Testing Mini Game/Raycast.cs
@@ -23,6 +23,7 @@ public class Raycast : MonoBehaviour
     private RaycastHit hit;
 
     public bool riverClicked = false;
+    public bool aPanelIsActive = false;
     public static bool isClickable = false;
 
     private void Update()
@@ -49,57 +50,66 @@ public class Raycast : MonoBehaviour
                         riverClicked = true; // Set bool riverClicked to true
                     }
 
-                    // Check if the clicked object has the "Trash: can" tag & bool effectsOfAluminumPanelActive is false
-                    if (hit.collider.CompareTag("Trash: can") && !WaterTestingManager.effectsOfAluminumPanelActive)
+                    
+                    if (!aPanelIsActive)
                     {
-                        Debug.Log("Aluminum can clicked"); // Debug.Log
-                        WaterTestingManager.effectsOfAluminumPanelActive = true; // Set bool effectsOfTrashPanelActive to true
-                        effectsOfAluminumPanel.SetActive(true); // Set the effectsOfTrashPanel to active
-                        WaterTestingManager.isAluminumCanObjectiveComplete = true; // Set bool isTrashBagObjectiveComplete to true
-                    }
-                    else
-                    {
-                        ;
-                    }
+                        // Check if the clicked object has the "Trash: can" tag & bool isAluminumCanObjectiveComplete is false
+                        if (hit.collider.CompareTag("Trash: can") && !WaterTestingManager.isAluminumCanObjectiveComplete)
+                        {
+                            Debug.Log("Aluminum can clicked"); // Debug.Log
+                            WaterTestingManager.effectsOfAluminumPanelActive = true; // Set bool effectsOfTrashPanelActive to true
+                            effectsOfAluminumPanel.SetActive(true); // Set the effectsOfTrashPanel to active
+                            WaterTestingManager.isAluminumCanObjectiveComplete = true; // Set bool isTrashBagObjectiveComplete to true
+                            aPanelIsActive = true; // Set bool aPanelIsActive to true
+                        }
+                        else
+                        {
+                            ;
+                        }
 
-                    // Check if the clicked object has the "Trash: tire" tag & bool effectsOfTirePanelActive is false
-                    if (hit.collider.CompareTag("Trash: tire") && !WaterTestingManager.effectsOfTirePanelActive)
-                    {
-                        Debug.Log("Tire clicked"); // Debug.Log
-                        WaterTestingManager.effectsOfTirePanelActive = true; // Set bool effectsOfTirePanelActive to true
-                        effectsOfTirePanel.SetActive(true); // Set the effectsOfTirePanel to active
-                        WaterTestingManager.isTireObjectiveComplete = true; // Set bool isTireObjectiveComplete to true
-                    }
-                    else
-                    {
-                        ;
-                    }
+                        // Check if the clicked object has the "Trash: tire" tag & bool isTireObjectiveComplete is false
+                        if (hit.collider.CompareTag("Trash: tire") && !WaterTestingManager.isTireObjectiveComplete)
+                        {
+                            Debug.Log("Tire clicked"); // Debug.Log
+                            WaterTestingManager.effectsOfTirePanelActive = true; // Set bool effectsOfTirePanelActive to true
+                            effectsOfTirePanel.SetActive(true); // Set the effectsOfTirePanel to active
+                            WaterTestingManager.isTireObjectiveComplete = true; // Set bool isTireObjectiveComplete to true
+                            aPanelIsActive = true; // Set bool aPanelIsActive to true
+                        }
+                        else
+                        {
+                            ;
+                        }
 
-                    // Check if the clicked object has the "Trash: gas can" tag & bool effectsOfGasPanelActive is false
-                    if (hit.collider.CompareTag("Trash: gas can") && !WaterTestingManager.effectsOfGasPanelActive)
-                    {
-                        Debug.Log("Gas canister clicked"); // Debug.Log
-                        WaterTestingManager.effectsOfGasPanelActive = true; // Set bool effectsOfGasPanelActive to true
-                        effectsOfGasPanel.SetActive(true); // Set the effectsOfGasPanel to active
-                        WaterTestingManager.isGasCanObjectiveComplete = true; // Set bool isGasCanObjectiveComplete to true
-                    }
-                    else
-                    {
-                        ;
-                    }
+                        // Check if the clicked object has the "Trash: gas can" tag & bool isGasCanObjectiveComplete is false
+                        if (hit.collider.CompareTag("Trash: gas can") && !WaterTestingManager.isGasCanObjectiveComplete)
+                        {
+                            Debug.Log("Gas canister clicked"); // Debug.Log
+                            WaterTestingManager.effectsOfGasPanelActive = true; // Set bool effectsOfGasPanelActive to true
+                            effectsOfGasPanel.SetActive(true); // Set the effectsOfGasPanel to active
+                            WaterTestingManager.isGasCanObjectiveComplete = true; // Set bool isGasCanObjectiveComplete to true
+                            aPanelIsActive = true; // Set bool aPanelIsActive to true
+                        }
+                        else
+                        {
+                            ;
 
-                    // Check if the clicked object has the "Trash: trash bag" tag & bool effectsOfTrashPanelActive is false
-                    if (hit.collider.CompareTag("Trash: trash bag") && !WaterTestingManager.effectsOfTrashPanelActive)
-                    {
-                        Debug.Log("Trash bag clicked"); // Debug.Log
-                        WaterTestingManager.effectsOfTrashPanelActive = true; // Set bool effectsOfTrashPanelActive to true
-                        effectsOfTrashPanel.SetActive(true); // Set the effectsOfTrashPanel to active
-                        WaterTestingManager.isTrashBagObjectiveComplete = true; // Set bool isTrashBagObjectiveComplete to true
-                    }
-                    else
-                    {
-                        ;
-                    }
+                        }
+
+                        // Check if the clicked object has the "Trash: trash bag" tag & bool isTrashBagObjective is false
+                        if (hit.collider.CompareTag("Trash: trash bag") && !WaterTestingManager.isTrashBagObjectiveComplete)
+                        {
+                            Debug.Log("Trash bag clicked"); // Debug.Log
+                            WaterTestingManager.effectsOfTrashPanelActive = true; // Set bool effectsOfTrashPanelActive to true
+                            effectsOfTrashPanel.SetActive(true); // Set the effectsOfTrashPanel to active
+                            WaterTestingManager.isTrashBagObjectiveComplete = true; // Set bool isTrashBagObjectiveComplete to true
+                            aPanelIsActive = true; // Set bool aPanelIsActive to true
+                        }
+                        else
+                        {
+                            ;
+                        }
+                    }     
                 }
             }
         }
