dfa43ecc32a79ab3a37c69cd976f30271d9f550c
Carly <cmeyerhardt@gmail.com>
2025-04-11 19:53:22 -0700

    Added looping playlist, Made DontDestroyOnLoad object Singleton

--- Assets/Scripts/Utility/PersistentObject.cs
+++ Assets/Scripts/Utility/PersistentObject.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
+using GneissUtilities;
 
 public class PersistentObject : MonoBehaviour
 {
-    private void Awake()
+    static PersistentObject instance;
+
+    private void OnEnable()
     {
-        DontDestroyOnLoad(gameObject);
+        gameObject.name = Time.time.ToString();
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 }
