8599bff16386db93638ea6baab9c8364c57c30c6
smrjewell <smrjewell@gmail.com>
2025-04-13 01:15:32 -0500

    Animal Collection changes
    added terrain
    
    adjusted scale of prefabs to make animals easier to click
    
    changed speed to be a random range between 2 variables
    
    changed spawning/ despawning of animals to be off screen
    
    adjusted spawn variables for x and z to fit within camera
    
    changed lose condition so that mini game restarts after a set amount of seconds
    
    added start screen panel and instructions

--- Assets/Scripts/Mini Game Scripts/Animal Collection/AnimalGameManager.cs
+++ Assets/Scripts/Mini Game Scripts/Animal Collection/AnimalGameManager.cs
@@ -7,25 +7,30 @@ public class AnimalGameManager : MonoBehaviour
     //UI Elements
     public TextMeshProUGUI ScoreText;
     public TextMeshProUGUI TimerText;
-    public TextMeshProUGUI TitleText;
+    public TextMeshProUGUI EndText;
+    public TextMeshProUGUI RetartingText;
+    public GameObject startPanel;
     public GameObject startButton;
     public GameObject returnButton;
     //Public Variables
     public GameObject[] animalPrefabs; //Add animals to this array, tag invasive animals with the invasive tag
-    public float spawnRangeX = 20; //How far apart the animals can spawn from the top to the bottom of the screen
-    public float spawnPosZ = -30; //how far to the left or right of the camera the animals spawn
+    public float spawnRangeX = 25; //How far apart the animals can spawn from the top to the bottom of the screen
+    public float spawnPosZ = -60; //how far to the left or right of the camera the animals spawn
     public float startDelay = 2; //Delay before animals start spawning after games begin
     public float spawnInterval = 0.5f; //Time between animal spawns (spawn rate)
     public float timeRemaining = 60f; //How long the game lasts
     public float ScoreThreshold = 25f; //Score to meet to win
     private bool timerRunning = false; //Game is 'active'
     public float Score = 0f; //tracks player score
+    public float restartTimer = 3f; //Time before game resets
 
     public static bool trappingCompleted = false; // Global variable to check if trapping is completed
 
     void Start()
     {
         returnButton.SetActive(false);
+        startButton.SetActive(true); //show start button
+        startPanel.SetActive(true); //show start panel
     }
 
     // Update is called once per frame
@@ -69,28 +74,39 @@ public class AnimalGameManager : MonoBehaviour
         ScoreText.text = "Score: " + Score.ToString() + "/" + ScoreThreshold.ToString();
         startButton.SetActive(false); //hide start button
         timerRunning = true; //start timer
-        TitleText.text = ""; //hide title text
+        startPanel.SetActive(false); //hide start panel
     }
 
     void EndLevel()
     {
         CancelInvoke("SpawnRandomAnimal"); //stop spawning animals
-        returnButton.SetActive(true);
-
+        
         if (Score < ScoreThreshold) //determine if the player won or lost
         {
-            TitleText.text = "You Lose!";
+            EndText.text = "You Lose!";
+            Invoke("RestartScene", restartTimer); //reset game after 2 seconds
         }
         else
         {
-            TitleText.text = "You Win!";
-        }
-
-        trappingCompleted = true; //set global variable to true
+            EndText.text = "You Win!";
+            returnButton.SetActive(true);
+            trappingCompleted = true; //set global variable to true
+        }  
     }
 
     public void ReturnButton()
     {
         SceneManager.LoadScene(0); //load main scene
+    }
+
+    public void RestartScene()
+    {
+        RetartingText.text = "Restarting in " + restartTimer.ToString() + " seconds...";
+        Invoke("Reload", restartTimer); //reload the game scene after x seconds
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name); //load the game scene
     }
 }
