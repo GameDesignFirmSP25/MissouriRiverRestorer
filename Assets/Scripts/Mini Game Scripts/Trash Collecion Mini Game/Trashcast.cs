c75bb963aa237dd50d8d7acd04c12f2692a403c6
AJS758 <146855739+AJS758@users.noreply.github.com>
2025-04-16 09:32:55 -0500

    Re addission of trash collection minigame clicking function

--- Assets/Scripts/Mini Game Scripts/Trash Collecion Mini Game/Trashcast.cs
+++ Assets/Scripts/Mini Game Scripts/Trash Collecion Mini Game/Trashcast.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class Trashcast : MonoBehaviour
@@ -34,40 +35,57 @@ public class Trashcast : MonoBehaviour
                 }
                 if (hit.collider.CompareTag("Trash: Styrofoam Cup")&& !TrashCollectionGame.ObjectveScup) 
                 {
-
+                    Debug.Log("Stryofoam cup clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for styrofoam cup
                 }
                 if (hit.collider.CompareTag("Trash: Bottle") && !TrashCollectionGame.ObjectvBottle)
                 {
-
+                    Debug.Log("Bottle clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for bottle 
                 }
                 if (hit.collider.CompareTag("Trash: trash bag")&& !TrashCollectionGame.ObjectvTrashBag)
                 {
-
+                    Debug.Log("Trash bag clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for trash bag
                 }
                 if (hit.collider.CompareTag("Trash: Pizza Slice")&& !TrashCollectionGame.ObjectvPizzaSlice)
                 {
-
+                    Debug.Log("Pizza slice clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for pizza slice
                 }
                 if (hit.collider.CompareTag("Trash: gas can")&& !TrashCollectionGame.ObjectvGasCan)
                 {
-
+                    DebugFrameTiming.Log("Gas can clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for gas can
                 }
                 if (hit.collider.CompareTag("Save bird") && !TrashCollectionGame.ObjectvSaveBird)
                 {
-
+                    Debug.Log("Save bird clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for save bird
                 }
                 if (hit.collider.CompareTag("Save fish") && !TrashCollectionGame.ObjectvSaveFish)
                 {
-
+                    Debug.Log("Save fish clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for save fish
                 }
                 if (hit.collider.CompareTag("Save deer") && !TrashCollectionGame.ObjectvSaveDeer)
                 {
-
+                    Debug.Log("Save deer clicked");
+                    Destroy(hit.collider.gameObject);// takes trash object off of the screen
+                    playerScore++; // adds score for save deer
                 }
 
 
                 if (hit.collider.CompareTag("TrashReceptical") &&  playerScore ==  8)
                 {
+                    Debug.Log("Trash Receptical clicked");
                     CollectedTrash += playerScore;
                     Debug.Log("Trash Collected: " +CollectedTrash);
                     
