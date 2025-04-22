46ce8f030d850e510aa4ce7f15cffac75a3ce06b
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2025-04-22 09:42:35 -0500

    Build error fix

--- Assets/Scripts/Mini Game Scripts/Animal Collection/AnimalGameManager.cs
+++ Assets/Scripts/Mini Game Scripts/Animal Collection/AnimalGameManager.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEditor.Experimental.GraphView;
-using System.Collections;
 using System.Collections.Generic;
 
 public class AnimalGameManager : BaseMiniGameManager
